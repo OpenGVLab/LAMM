7d60d0468492ade8a08cb2ba487838e42d281f53
ZHELUN SHI <shizhelun20000820@126.com>
2023-06-12 15:51:23 +0800

    feature: GPT Metric

---
+++ src/scripts/GPT_metric.sh
@@ -0,0 +1,5 @@
+python gpt_eval.py \
+        --base-data-path LAMM-Dataset/2D_Benchmark \
+        --answer-dir LAMM/answers/ \
+        --compare-dir LLaVA/answers \
+        --response-dir results
