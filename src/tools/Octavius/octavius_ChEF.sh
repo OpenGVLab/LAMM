8a1424a75c347ecec076f5f11ba1b7d154ec6570
chenzeren <czr.cn.525@gmail.com>
2023-11-06 18:17:27 +0800

    update sh

--- src/tools/Octavius/octavius_ChEF.sh
+++ src/tools/Octavius/octavius_ChEF.sh
@@ -1,9 +1,9 @@
 # choose one model_cfg from 2d, 3d, 2d+3d
 model_cfg=config/ChEF/models/octavius_2d.yaml
-recipe_cfg_list=(CIFAR10 Flickr30k CelebA_hair CelebA_smile VOC2012 ScienceQA)
+recipe_cfg_list=(CIFAR10)
 
 for dataset in ${recipe_cfg_list[*]}; do
-    srun -p <YOUR_PARTITION> --gres=gpu:1 --ntasks-per-node=1 --kill-on-bad-exit \
+    srun -p AI4Good_X --gres=gpu:1 --ntasks-per-node=1 --kill-on-bad-exit \
         python eval.py \
             --model_cfg ${model_cfg} \
             --recipe_cfg config/ChEF/scenario_recipes/LAMM/${dataset}.yaml
