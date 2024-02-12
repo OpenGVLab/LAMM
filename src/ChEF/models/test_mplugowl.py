87a2507aa623e8c0d7e1d35519e65f664f578568
Coach257 <shizhelun20000820@126.com>
2024-02-12 14:30:30 +0800

    otter

--- src/ChEF/models/test_mplugowl.py
+++ src/ChEF/models/test_mplugowl.py
@@ -29,7 +29,7 @@ class TestMplugOwl(TestBase):
         self.model.eval()
 
     def build_conversation(self, idx, image_list, prompt, CoT_answer_list=None, batch_answers=None, **kwargs):
-        lenimg = 2 if isinstance(image_list,str) else len(image_list)
+        lenimg = 1 if isinstance(image_list,str) else len(image_list)
         prompt = prompt_template_multi + "Human: ".join(["<image>"] * lenimg) + f"\nHuman: {prompt}\nAI:"
         if CoT_answer_list is not None:
             prompt += ' ' + CoT_answer_list[idx]
